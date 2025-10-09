5fdcf5f1e2423722e219502ca8ab43ce073d0c4e
vatsalyaexpertsolutions <vatsalyaexpertsolutions@gmail.com>
2025-10-09 09:33:42 +0000

    array changes

--- testing/demo.js
+++ testing/demo.js
@@ -1,1 +1,30 @@
-console.log("demo");
+//  1. Parentheses ( )
+
+//  2. Brackets [ ]
+
+//  3. Curly Braces { }
+
+
+
+///////////////////////////////
+
+
+// array
+
+const myArr = [1, 2, 3, 4, 5]
+
+
+myArr.push(6); // last me ek value add hogi 
+myArr.push(7);
+
+myArr.pop();  //jo bhi last value hogi usko remove kar dega
+
+
+myArr.unshift(9)  // startin me ek value add ho jayegi
+
+
+
+//   console.log(myArr.includes(9));  // includes ke Brackets me jo value likhi he wo array me he kya check karke karta he true 
+
+
+console.log(myArr.includes(9));
